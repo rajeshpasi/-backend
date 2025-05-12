8e8cc674c152f3a6d5c1f2ff509a1aeb6a67e454
Rajesh Kumar Pasi <imvip2002@gmail.com>
2025-05-12 15:59:05 +0530

    err

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,30 +1,33 @@
 import jwt from "jsonwebtoken";
 import { ApiError } from "../utils/ApiError.js";
-import { asyncHandler } from "./asyncHandler.js";
-import User from "../models/user.model.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
- try {
-   const token = req.cookies?.accessToken || req.headers("authorization")?.replace("Bearer ", "");
- 
-   if (!token) {
-     throw new ApiError(401, "unauthorized request");
-   }
- 
-   const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-   req.user = await User.findById(decoded?._id).select("-password -refreshToken");
- 
-   if (!req.user) {
-     throw new ApiError(401, "invalid access token");
-   }
- 
-   req.user = user;
-   next();
- } catch (error) {
-  throw new ApiError(401, error.message || "Something went wrong while verifying access token");
- }
+  try {
+    const token =
+      req.cookies?.accessToken ||
+      req.headers("authorization")?.replace("Bearer ", "");
 
+    if (!token) {
+      throw new ApiError(401, "unauthorized request");
+    }
+
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    req.user = await User.findById(decoded?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!req.user) {
+      throw new ApiError(401, "invalid access token");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(
+      401,
+      error.message || "Something went wrong while verifying access token"
+    );
+  }
 });
-
-
-
